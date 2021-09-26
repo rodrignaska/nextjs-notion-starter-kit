d0f7cebb054e88500b1967792ba55aa700409aaa
rodrignaska <64367446+rodrignaska@users.noreply.github.com>
2021-09-25 17:33:15 -0700

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,25 +1,25 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '78fc5a4b88d74b0e824e29407e9f1ec1',
+  rootNotionPageId: '07bce3868a5140ca88187c30d6a591d2',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Transitive Bullshit',
-  domain: 'transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Upgraded - Internal Docs',
+  domain: 'docs.upgraded.dev',
+  author: 'Upgraded Product Studio LLC',
 
   // open graph metadata (optional)
-  description: 'Example site description',
-  socialImageTitle: 'Transitive Bullshit',
-  socialImageSubtitle: 'Hello World! 👋',
+  description: 'The internal docs space for Upgraded',
+  socialImageTitle: 'The internal docs space for Upgraded',
+  socialImageSubtitle: 'The internal docs space for Upgraded',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  twitter: 'upgraded_dev',
+  github: 'upgraded_dev',
+  linkedin: 'rodrignaska',
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
